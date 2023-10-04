43a93195ff773afa089d8c789e2a33ebf30068e0
LordYoungblood <ParissYoungblood@gmail.com>
2023-10-04 15:13:12 -0400

    setting up the frontend. I have created the Ticket Details and the TicketForm learning vue

--- Ticket-Application-Frontend/src/App.vue
+++ Ticket-Application-Frontend/src/App.vue
@@ -1,17 +1,20 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import TicketDetails from "./components/TicketDetails.vue";
+import TicketForm from "./components/TicketForm.vue";
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <!-- Using the TicketDetails and TicketForm components -->
+    <TicketDetails
+      category="Sample Category"
+      :types="['Type1', 'Type2']"
+      subject="Sample Subject"
+      description="Sample Description"
+      :files="['file1.txt', 'file2.txt']"
+    />
+    <TicketForm />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
